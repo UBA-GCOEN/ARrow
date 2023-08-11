157cc3f74e8152937b33f9b233b8a15cab509ac3
Naresh <chandanbatven@gmail.com>
2023-08-12 03:06:12 +0530

    refixed

--- server/src/index.js
+++ server/src/index.js
@@ -6,13 +6,19 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+const  CONNECTION_URI = process.env.MONGODB_URI;
 
 const app = express()
 
 import indexRoute from "./api/routes/index.js";
 app.use(indexRoute)
 
-
-app.listen(PORT, () => {
-    console.log("server running on port "+PORT)
-});
+mongoose
+.connect(CONNECTION_URI,  { useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>{
+    app.listen(PORT, () => {
+        console.log("server running on port "+PORT)
+    });
+})
+.catch((err) => console.log(err.message));
+    
