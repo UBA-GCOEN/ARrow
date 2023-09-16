990f922bab79f38ed71b7d659526a3dbfcfc3c2a
Naresh <chandanbatven@gmail.com>
2023-09-16 20:26:12 +0530

    improved session on all user
    Signed-off-by: Naresh <chandanbatven@gmail.com>

--- server/src/api/controllers/userStudent.js
+++ server/src/api/controllers/userStudent.js
@@ -206,6 +206,12 @@ export const signin = async (req, res) => {
     }
   }
   else{
+    req.session.destroy(err => {
+      if (err) {
+        console.error("Error destroying session:", err);
+        res.status(500).send("Internal Server Error");
+      } 
+    });
     res.json({msg:"User Student does not exist."})
   }
 }
